314aa009e4c55bf481374ed4754fa9c0689cd903
Simpy <kodi03466@hotmail.com>
2026-04-20 20:35:05 -0400

    API: game menu button misplaced (mist ptr)

--- ElvUI/Game/Shared/General/API.lua
+++ ElvUI/Game/Shared/General/API.lua
@@ -1165,7 +1165,7 @@ function E:PositionGameMenuButton()
 
 			local lastIndex = gameMenuLastButtons[text]
 			if lastIndex == gameMenuLastButtons.ElvUI and GameMenuFrame.ElvUI then
-				GameMenuFrame.ElvUI:Point('TOPLEFT', button, 'BOTTOMLEFT', 0, (E.Retail and -45) or (E.Mists and -20) or -10)
+				GameMenuFrame.ElvUI:Point('TOPLEFT', button, 'BOTTOMLEFT', 0, (E.Retail and -45) or (E.Mists and -30) or -10)
 			elseif not lastIndex then
 				button:NudgePoint(nil, -offset)
 			end
