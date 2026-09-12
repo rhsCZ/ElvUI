rhsCZ/ElvUI

commit 3b1d9147f15a34b42b8e650d9a918fbe1521a30b

raidrole: adjust assist icon again

ElvUI_Libraries/Game/Shared/oUF/elements/assistantindicator.lua
@@ -42,14 +42,12 @@ local function Update(self, event)
 		element:PreUpdate()
 	end
 
-	local isAssist = UnitIsGroupAssistant(unit)
-	local isLeader = UnitIsGroupLeader(unit)
-	local isSecret = oUF:IsSecretValue(isAssist) or oUF:IsSecretValue(isLeader)
-	local isAssistant = not isSecret and (isAssist and not isLeader)
-	if not isAssistant or (element.combatHide and UnitAffectingCombat(unit)) then
-		element:Hide()
+	local isAssistant = UnitIsGroupAssistant(unit) and not UnitIsGroupLeader(unit)
+	local combatHide = element.combatHide and UnitAffectingCombat(unit)
+	if element.SetAlphaFromBoolean then
+		element:SetAlphaFromBoolean(isAssistant, not combatHide and 1 or 0, 0)
 	else
-		element:Show()
+		element:SetAlpha(not combatHide and isAssistant and 1 or 0)
 	end
 
 	--[[ Callback: AssistantIndicator:PostUpdate(isAssistant)
@@ -100,7 +98,7 @@ end
 local function Disable(self)
 	local element = self.AssistantIndicator
 	if(element) then
-		element:Hide()
+		self:SetAlpha(0)
 
 		self:UnregisterEvent('UNIT_FLAGS', Path)
 		self:UnregisterEvent('GROUP_ROSTER_UPDATE', Path)

ElvUI/Game/Shared/Modules/UnitFrames/Elements/RaidRoleIcons.lua
@@ -65,8 +65,8 @@ function UF:RaidRoleUpdate()
 	if db then
 		local leader = frame.LeaderIndicator
 		local assistant = frame.AssistantIndicator
-		local masterlooter = frame.MasterLooterIndicator
-		local mamt = frame.RaidRoleIndicator
+		local masterLooter = frame.MasterLooterIndicator
+		local raidRole = frame.RaidRoleIndicator
 
 		local pos, x, y = db.position or 'TOPLEFT', db.xOffset or 0, db.yOffset or 4
 		local size = 12 * (db.scale or 1)
@@ -75,46 +75,29 @@ function UF:RaidRoleUpdate()
 		local pos1 = right and 'RIGHT' or 'LEFT'
 		local pos2 = right and 'LEFT' or 'RIGHT'
 
-		local isLeader = leader:IsShown()
-		local isAssist = assistant:IsShown()
-		local isMAMT = mamt:IsShown()
-		local isML = masterlooter:IsShown()
-
-		leader:ClearAllPoints()
-		assistant:ClearAllPoints()
-		masterlooter:ClearAllPoints()
-		mamt:ClearAllPoints()
-
 		leader:Size(size)
+		leader:ClearAllPoints()
+		leader:Point(pos, anchor, x, y)
+
 		assistant:Size(size)
-		masterlooter:Size(size)
-		mamt:Size(size)
+		assistant:ClearAllPoints()
+		assistant:Point(pos, anchor, x, y)
 
-		if isLeader then
-			leader:Point(pos, anchor, x, y)
-		elseif isAssist then
-			assistant:Point(pos, anchor, x, y)
+		local isMAMT = raidRole:IsShown()
+		if isMAMT then -- assist / tank
+			raidRole:Size(size)
+			raidRole:ClearAllPoints()
+			raidRole:Point(pos1, leader, pos2)
 		end
 
-		if isMAMT then
-			if isLeader then
-				mamt:Point(pos1, leader, pos2)
-			elseif isAssist then
-				mamt:Point(pos1, assistant, pos2)
-			else
-				mamt:Point(pos, anchor, x, y)
-			end
-		end
+		if masterLooter:IsShown() then
+			masterLooter:Size(size)
+			masterLooter:ClearAllPoints()
 
-		if isML then
 			if isMAMT then
-				masterlooter:Point(pos1, mamt, pos2)
-			elseif isLeader then
-				masterlooter:Point(pos1, leader, pos2)
-			elseif isAssist then
-				masterlooter:Point(pos1, assistant, pos2)
+				masterLooter:Point(pos1, raidRole, pos2)
 			else
-				masterlooter:Point(pos, anchor, x, y)
+				masterLooter:Point(pos1, leader, pos2)
 			end
 		end
 	end

ElvUI_Libraries/Game/Shared/oUF/elements/leaderindicator.lua
@@ -124,7 +124,7 @@ end
 local function Disable(self)
 	local element = self.LeaderIndicator
 	if(element) then
-		element:Hide()
+		self:SetAlpha(0)
 
 		self:UnregisterEvent('UNIT_FLAGS', Path)
 		self:UnregisterEvent('PLAYER_FLAGS_CHANGED', Path)

commit edebd8bffcce4c6688254cefd7f6bde720f919bc

oUF: assist secret check broken by 3b1d9147f1

ElvUI_Libraries/Game/Shared/oUF/elements/assistantindicator.lua
@@ -26,7 +26,6 @@ local _, ns = ...
 local oUF = ns.oUF
 
 local UnitIsGroupAssistant = UnitIsGroupAssistant
-local UnitIsGroupLeader = UnitIsGroupLeader
 local UnitAffectingCombat = UnitAffectingCombat
 
 local function Update(self, event)
@@ -42,7 +41,7 @@ local function Update(self, event)
 		element:PreUpdate()
 	end
 
-	local isAssistant = UnitIsGroupAssistant(unit) and not UnitIsGroupLeader(unit)
+	local isAssistant = UnitIsGroupAssistant(unit)
 	local combatHide = element.combatHide and UnitAffectingCombat(unit)
 	if element.SetAlphaFromBoolean then
 		element:SetAlphaFromBoolean(isAssistant, not combatHide and 1 or 0, 0)